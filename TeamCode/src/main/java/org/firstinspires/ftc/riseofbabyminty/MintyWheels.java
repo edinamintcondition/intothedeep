6ca6516daa3924cde4e96980dda87acf1dbcdee2
thechickenmaster <pollito.chicken.red@gmail.com>
2024-10-22 19:48:34 -0500

    embarking on a function quest
    waaah

--- TeamCode/src/main/java/org/firstinspires/ftc/riseofbabyminty/MintyWheels.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/riseofbabyminty/MintyWheels.java
@@ -37,7 +37,7 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 /*
  */
 
-@TeleOp(name="MintyWheels", group="Linear OpMode")
+@TeleOp(name = "MintyWheels", group = "Linear OpMode")
 
 public class MintyWheels extends LinearOpMode {
 
@@ -53,8 +53,8 @@ public class MintyWheels extends LinearOpMode {
 
         // Initialize the hardware variables. Note that the strings used here must correspond
         // to the names assigned during the robot configuration step on the DS or RC devices.
-        leftFrontDrive  = hardwareMap.get(DcMotor.class, "left_front_drive");
-        leftBackDrive  = hardwareMap.get(DcMotor.class, "left_back_drive");
+        leftFrontDrive = hardwareMap.get(DcMotor.class, "left_front_drive");
+        leftBackDrive = hardwareMap.get(DcMotor.class, "left_back_drive");
         rightFrontDrive = hardwareMap.get(DcMotor.class, "right_front_drive");
         rightBackDrive = hardwareMap.get(DcMotor.class, "right_back_drive");
 
@@ -70,31 +70,31 @@ public class MintyWheels extends LinearOpMode {
         waitForStart();
         runtime.reset();
 
-       double forwardPowerLimit = 0.60;
-       double backwardPowerLimit = -0.60;
+        double forwardPowerLimit = 0.60;
+        double backwardPowerLimit = -0.60;
 
 
         while (opModeIsActive()) {
             double max;
 
             // POV Mode uses left joystick to go forward & strafe, and right joystick to rotate.
-            double axial   = -gamepad1.left_stick_y;  // Note: pushing stick forward gives negative value
-            double lateral =  gamepad1.left_stick_x;
-            double yaw     =  gamepad1.right_stick_x;
+            double axial = -gamepad1.left_stick_y;  // Note: pushing stick forward gives negative value
+            double lateral = gamepad1.left_stick_x;
+            double yaw = gamepad1.right_stick_x;
 
             // Combine the joystick requests for each axis-motion to determine each wheel's power.
             // Set up a variable for each drive wheel to save the power level for telemetry.
-            double leftFrontPower  = axial + lateral + yaw;
+            double leftFrontPower = axial + lateral + yaw;
             double rightFrontPower = axial - lateral - yaw;
-            double leftBackPower   = axial - lateral + yaw;
-            double rightBackPower  = axial + lateral - yaw;
+            double leftBackPower = axial - lateral + yaw;
+            double rightBackPower = axial + lateral - yaw;
 
             // Normalize the values so no wheel power exceeds 100%
             // This ensures that the robot maintains the desired motion.
             max = Math.max(Math.abs(leftFrontPower), Math.abs(rightFrontPower));
             max = Math.max(max, Math.abs(leftBackPower));
             max = Math.max(max, Math.abs(rightBackPower));
-
+//below is in disuse
             if (gamepad1.right_bumper) {
                 forwardPowerLimit = 1.0;
                 backwardPowerLimit = -1;
@@ -110,10 +110,10 @@ public class MintyWheels extends LinearOpMode {
             }
 
             if (max > 1.0) {
-                leftFrontPower  /= max;
+                leftFrontPower /= max;
                 rightFrontPower /= max;
-                leftBackPower   /= max;
-                rightBackPower  /= max;
+                leftBackPower /= max;
+                rightBackPower /= max;
             }
             // Send calculated power to wheels
             leftFrontDrive.setPower(leftFrontPower);
@@ -126,5 +126,7 @@ public class MintyWheels extends LinearOpMode {
             telemetry.addData(">", "BackRight motor: ", rightBackPower);
             telemetry.update();
         }
-    }}
+
+    }
+}
 // wololo
